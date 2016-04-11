c47f68156814f7b6369e21d483fa6497dceb6fd4
Liu, Zheng <zheng.l.liu@intel.com>
2016-04-11 14:07:45 +0800

    Change Rule class

--- server/myClasses/Rule.py
+++ server/myClasses/Rule.py
@@ -6,14 +6,21 @@
 __author__ = 'Sodablueblue'
 
 class Rule(object):
-	pass
-
-class Restriction(Rule):
 	def __init__(self, type, criteria):
 		self.criteria = criteria
 		self.type = type
 
+	def render(self):
+		return self.type + ' ' + self._toHtml()
+
+class Restriction(Rule):
+	def __init__(self, type, criteria):
+		super(Restriction, self).__init__(type, criteria)
+
 	def _checkCriteria(self, value):
+		pass
+
+	def _toHtml(self):
 		pass
 
 	def check(self, values, bench = None):
@@ -24,8 +31,7 @@ class Restriction(Rule):
 
 class Combination(Rule):
 	def __init__(self, type, criteria):
-		self.criteria = criteria
-		self.type = type
+		super(Restriction, self).__init__(type, criteria)
 
 	def _checkCriteria(self, value):
 		pass
@@ -33,9 +39,12 @@ class Combination(Rule):
 	def _checkbench(self, value, bench):
 		pass
 
+	def _toHtml(self):
+		pass
+
 	def check(self, values, bench):
 		try:
 			self._checkCriteria(values)
 			self._checkBench(values, bench)
 		except BaseException(e):
-			raise BaseException('Combination ', self.__class__.name__.': ', e)
+			raise BaseException('Combination ', self.__class__.name__,': ', e)
